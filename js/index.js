2b0364eceafc46fb4742a8e97f0d3e22ceba3cd4
headsink <headsink@gmail.com>
2022-11-10 18:41:08 +0800

    made it simpler because it works

--- js/index.js
+++ js/index.js
@@ -1,4 +1,4 @@
-// import the class
+
 const canvas = document.querySelector('canvas');
 const c = canvas.getContext('2d');
 canvas.width = 1024;
@@ -6,4 +6,12 @@ canvas.height = 576;
 
 c.fillRect(0, 0, canvas.width, canvas.height);
 
-// summon the class
+class Sprite {
+    constructor(position) {
+        this.position = position;
+    }
+}
+
+const player = new Sprite({x:50,y:100});
+c.fillStyle = 'red';
+c.fillRect(player.position.x,player.position.y,10,10);
