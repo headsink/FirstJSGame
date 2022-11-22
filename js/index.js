bcb92c3c1007c1db5e154c04555a7231318700ca
headsink <headsink@gmail.com>
2022-11-22 18:11:52 +0800

    add more properties for sprite

--- js/index.js
+++ js/index.js
@@ -1,17 +1,39 @@
 
 const canvas = document.querySelector('canvas');
 const c = canvas.getContext('2d');
+
 canvas.width = 1024;
 canvas.height = 576;
 
 c.fillRect(0, 0, canvas.width, canvas.height);
 
+const gravity = 0.2;
+
 class Sprite {
-    constructor(position) {
+    constructor({ position, velocity }) {
         this.position = position;
+        this.velocity = velocity;
+        this.height = 150;
+    }
+
+    draw() {
+        c.fillStyle = 'red';
+        c.fillRect(this.position.x, this.position.y, 50, this.height);
+    }
+    update() {
+        this.draw();
     }
 }
 
-const player = new Sprite({x:50,y:100});
+const player = new Sprite({
+    position: {
+        x: 0,
+        y: 0
+    },
+    velocity: {
+        x: 0,
+        y: 0
+    }
+});
 c.fillStyle = 'red';
 c.fillRect(player.position.x,player.position.y,10,10);
